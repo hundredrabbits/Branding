67be1d81d4436e7b327e25415e1c2b1e9308b730
Devine Lu Linvega <aliceffekt@gmail.com>
2015-11-12 11:37:56 -0500

    First version of the animated logo

--- Logo/Logo/GameScene.swift
+++ Logo/Logo/GameScene.swift
@@ -4,10 +4,12 @@
 
 import SpriteKit
 
+let rabbits = SKNode()
+var logo:SKLabelNode!
+var steps = 0
+
 class GameScene: SKScene
 {
-	let rabbits = SKNode()
-	
     override func didMoveToView(view: SKView)
 	{
 		var x = 0
@@ -23,8 +25,50 @@ class GameScene: SKScene
 		}
 		addChild(rabbits)
 		rabbits.position = CGPoint(x:CGRectGetMidX(self.frame) - (25*10)/2,y:CGRectGetMidY(self.frame) - (25*10)/2)
-    }
-    
+		
+		scare()
+		scare()
+		scare()
+		scare()
+		scare()
+		scare()
+		
+		NSTimer.scheduledTimerWithTimeInterval(0.5, target: self, selector: "call", userInfo: nil, repeats: true)
+		
+		logo = SKLabelNode(fontNamed: "Alte Haas Grotesk Bold")
+		logo.position = CGPoint(x:CGRectGetMidX(self.frame) - 12.5,y:CGRectGetMidY(self.frame) - (25 * 8))
+		logo.horizontalAlignmentMode = .Center
+		logo.text = "hundredrabbits"
+		logo.fontSize = 20
+		logo.alpha = 0
+		addChild(logo)
+	}
+	
+	func displayLogo()
+	{
+		let action_fade = SKAction.fadeAlphaTo(1, duration: 1)
+		logo.runAction(action_fade)
+	}
+	
+	func scare()
+	{
+		for case let rabbit as Rabbit in rabbits.children {
+			rabbit.flee()
+		}
+	}
+	
+	func call()
+	{
+		steps += 1
+		for case let rabbit as Rabbit in rabbits.children {
+			rabbit.sneak()
+		}
+		
+		if steps == 8 {
+			displayLogo()
+		}
+	}
+	
     override func update(currentTime: CFTimeInterval)
 	{
         /* Called before each frame is rendered */
--- Logo/Logo/Rabbit.swift
+++ Logo/Logo/Rabbit.swift
@@ -10,19 +10,77 @@ class Rabbit : SKNode
 	var x:Int!
 	var y:Int!
 	var sprite:SKShapeNode!
+	var moves:Array<CGPoint> = []
 	
 	init(x:Int,y:Int)
 	{
 		super.init()
 		
+		self.x = x
+		self.y = y
+		
 		sprite = SKShapeNode(circleOfRadius: 10)
 		sprite.fillColor = NSColor.whiteColor()
+		sprite.strokeColor = NSColor.blackColor()
 		addChild(sprite)
 		position = CGPoint(x: x * 25,y: y * 25)
+		moves.append(position)
+	}
+	
+	func flee()
+	{
+		let direction = Direction.random()
+		
+		var test_x = 0
+		var test_y = 0
+		
+		if direction == Direction.Right { test_x = 1  }
+		if direction == Direction.Left  { test_x = -1 }
+		if direction == Direction.Up    { test_y = 1  }
+		if direction == Direction.Down  { test_y = -1 }
+		
+		var canMove:Bool = true
+		
+		for rabbit in rabbits.children {
+			let rabbit = rabbit as! Rabbit
+			if test_x != 0 && rabbit.x == x + test_x && rabbit.y == y { canMove = false }
+			if test_y != 0 && rabbit.x == x && rabbit.y == y + test_y { canMove = false }
+		}
+		
+		if canMove == true {
+			x = x + test_x
+			y = y + test_y
+			position = CGPoint(x: x * 25,y: y * 25)
+		}
+		moves.append(position)
+	}
+	
+	func sneak()
+	{
+		if moves.count < 1 { return }
+		let action_move = SKAction.moveTo(moves.last!, duration: 0.45)
+		action_move.timingMode = .EaseInEaseOut
+		runAction(action_move)
+		
+		moves.removeLast()
 	}
 
 	required init?(coder aDecoder: NSCoder)
 	{
 	    fatalError("init(coder:) has not been implemented")
+	}
+}
+
+enum Direction: UInt32
+{
+	case Up
+	case Down
+	case Left
+	case Right
+	
+	static func random() -> Direction
+	{
+		let rand = arc4random_uniform(Right.rawValue+1)
+		return Direction(rawValue: rand)!
 	}
 }
